dc05906fc985f71c1ac4aa5a335bdf79bebce52e
Gaurav Tewari <gauravtewari111@gmail.com>
2021-12-06 15:12:30 +0530

    chore: bug fix

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -7,16 +7,16 @@ import makeFirstLetterCapital from "../utils/heading";
  export default function Categories (){
      const route  = useRouter();
      const { slug } =  route.query;
-     var Heading = "";
-     if(slug){
-         Heading = makeFirstLetterCapital(slug);
-     } 
+    //  var Heading = "";
+    //  if(slug){
+    //      Heading = makeFirstLetterCapital(slug);
+    //  } 
     
     return(
         <>
         <NavBar/>
           <Heading ml="5%" paddingTop="3%" paddingBottom="2%">
-          {Heading} wear
+          {slug} wear
           <Divider width="20%"   marginTop="1%"/>
           </Heading> 
         <Footer/>
