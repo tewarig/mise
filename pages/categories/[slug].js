2372c0232e4b9aa5093d6332aa2298b2c15a6689
Gaurav Tewari <gauravtewari111@gmail.com>
2021-12-09 12:46:28 +0530

    feat: routes added

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,25 +1,51 @@
-import Footer from "../comp/footer"
-import NavBar from "../comp/navbar"
+import { useEffect, useState } from "react";
+import Footer from "../comp/footer";
+import NavBar from "../comp/navbar";
 import { useRouter } from "next/router";
 import { Heading, Divider } from "@chakra-ui/react";
-import makeFirstLetterCapital from "../../utils/heading";
+import { getRouteMatcher } from "next/dist/shared/lib/router/utils";
 
- export default function Categories (){
-     const route  = useRouter();
-     const { slug } =  route.query;
-    //  var Heading = "";
-    //  if(slug){
-    //      Heading = makeFirstLetterCapital(slug);
-    //  } 
-    
-    return(
-        <>
-        <NavBar/>
-          <Heading ml="5%" paddingTop="3%" paddingBottom="2%">
-          {slug} wear
-          <Divider width="20%"   marginTop="1%"/>
-          </Heading> 
-        <Footer/>
-        </>
-    )
+export default function Categories({ data }) {
+  console.log(data);
+  const route = useRouter();
+  const { slug } = route.query;
+
+  return (
+    <>
+      <NavBar />
+      <Heading ml="5%" paddingTop="3%" paddingBottom="2%">
+        {slug} wear
+        <Divider width="20%" marginTop="1%" />
+      </Heading>
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const request = await fetch(
+    "http://localhost:3000/api/categories/" + params.slug
+  );
+  const data = await request.json();
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: "winter" } },
+      { params: { slug: "party" } },
+      { params: { slug: "beach" } },
+      { params: { slug: "traditional" } },
+    ],
+    fallback: false,
+  };
 }
