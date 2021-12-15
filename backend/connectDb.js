da7b920804e14b4570bf901fb7423f03b4b48014
Gaurav <hello@gauravtewar.xyz>
2021-12-15 16:24:18 +0530

    feat: connect db added

---
+++ backend/connectDb.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+async function connectDb(){
+  try{
+    await mongoose.connect(process.env.dbUrl,{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+    })
+    console.log('connected to database');
+  }
+  catch(error){
+   console.log(error);
+  }
+}
+module.exports = connectDb;
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const express = require("express");
+const cros = require("cors");
+const connectDb = require('./connectDb');
+
+
+
+const app = express();
+const port = process.env.PORT||4000;
+connectDb();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.listen(port,()=>(console.log(`server have started on port ${port}`)));
