9df4d9752eea4858ff6044c9707d760a1365298f
Gaurav <hello@gauravtewar.xyz>
2021-12-17 11:15:38 +0530

    feat: chore paypal drawer added

--- pages/checkout.js
+++ pages/checkout.js
@@ -7,18 +7,38 @@ import NavBar from "./comp/navbar";
 import { useState, useEffect } from "react";
 import Script from "next/script";
 import { PayPalButton } from "react-paypal-button-v2";
-
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure
+} from "@chakra-ui/react";
+import React from "react";
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
   const [productsData, setProductsData] = useState();
-  const [scriptLoaded , setScriptLoaded] = useState(false);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  const [name, setName] = useState();
+  const [phoneNumer, setPhoneNumber] = useState();
+  const [address, setAddress] = useState();
+  const [pin, setPin] = useState();
+  const [state, setState] = useState();
 
-      setTimeout(()=>{
-        setScriptLoaded(true);
 
-      },3000)
   
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  setTimeout(() => {
+    setScriptLoaded(true);
+  }, 3000);
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
@@ -36,10 +56,61 @@ export default function Index() {
 
     var count = {};
   };
+  
+   const createOrder = () => {
+       onOpen();
+       console.log(name);
+       console.log(phoneNumer);
+       console.log(address);
+       console.log(pin);
+       console.log(state);
+   }
 
   if (user) {
     return (
       <>
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Payment Methords avaible</DrawerHeader>
+
+            <DrawerBody>
+            {scriptLoaded && (
+                <Box alignContent="center" alignSelf="center">
+                  <PayPalButton
+                    alignSelf="center"
+                    amount={price()}
+                    onSuccess={(details, data) => {
+                      alert(
+                        "Transaction completed by " +
+                          details.payer.name.given_name
+                      );
+
+                      return fetch("/paypal-transaction-complete", {
+                        method: "post",
+                        body: JSON.stringify({
+                          orderID: data.orderID,
+                        }),
+                      });
+                    }}
+                  />
+                </Box>
+              )}
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
         <Script
           src="https://www.paypal.com/sdk/js?client-id=AYIJQ2eYR1lxMuTgUaagVH2h77NJpu229ZZiI1s6zBX3eqiDUImiecc-6lU-3g7NeFQ2s3ihTdyXCOnR"
           type="text/javascript"
@@ -61,38 +132,26 @@ export default function Index() {
               we process your order.
             </Box>
             <Box margin="4%">
-              <Input margin="1%" placeholder="Receptionist Name"></Input>
+              <Input margin="1%" placeholder="Receptionist Name" value={name} onChange={e => setName(e.target.value)}></Input>
               <Input
                 margin="1%"
                 placeholder="Phone Number"
                 type="number"
+                onChange={e => setPhoneNumber(e.target.value)}
               ></Input>
-              <Input margin="1%" placeholder="Address" />
-              <Input margin="1%" placeholder="pin" type="number" />
-              <Input margin="1%" placeholder="State" />
+              <Input margin="1%" placeholder="Address"  onChange={e => setAddress(e.target.value)}/>
+              <Input margin="1%" placeholder="pin" type="number" onChange={e => setPin(e.target.value)} />
+              <Input margin="1%" placeholder="State"  onChange={e => setState(e.target.value)}/>
             </Box>
             <Flex margin="5%" direction="row" justifyContent="space-between">
               <Box alignSelf="flex-end">
-                <Button> Pay Now </Button>
+                <Button ref={btnRef} colorScheme="teal" onClick={createOrder}>
+                  Pay Now
+                </Button>
               </Box>
               <Heading> ${price()} </Heading>
+              
             </Flex>
-
-            {scriptLoaded && 
-            <PayPalButton
-        amount={price()}
-        onSuccess={(details, data) => {
-          alert("Transaction completed by " + details.payer.name.given_name);
-
-          return fetch("/paypal-transaction-complete", {
-            method: "post",
-            body: JSON.stringify({
-              orderID: data.orderID
-            })
-          });
-        }}
-      />
-    }
           </Box>
         </div>
         <Footer></Footer>
@@ -100,5 +159,21 @@ export default function Index() {
     );
   }
 
-  return <a href="/api/auth/login">Login</a>;
+  return (
+    <>
+      {" "}
+      <NavBar />
+      <Box align="center">
+        <a href="/api/auth/login">
+          <img src="https://media.istockphoto.com/photos/please-stop-picture-id180835523?k=20&m=180835523&s=612x612&w=0&h=RyPGPCnuCT0vtqPj69ExiaBTMIjb7ErYfewMFXnJjOY=" />
+          <Heading>
+            {" "}
+            You can proceed any further you need to login to place your order{" "}
+          </Heading>
+          <Button margin="5%"> Login </Button>
+        </a>{" "}
+      </Box>
+      <Footer />{" "}
+    </>
+  );
 }
